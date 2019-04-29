110ae1d44bc9c025e37ddb0dfb16497f6ad45580
lucasliu08 <40473190+lucasliu08@users.noreply.github.com>
2019-04-29 08:36:54 -0300

    Update conf.js

--- protractorBase/conf.js
+++ protractorBase/conf.js
@@ -3,8 +3,6 @@ exports.config = {
   
   directConnect: true,
 
-  //The adress of a running selenium server.
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
